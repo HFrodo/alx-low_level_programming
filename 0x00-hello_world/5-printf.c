f0a042da0f24ef8b2d77e43ebbe202f53bdc4b00
HFrodo <egyepong10@gmail.com>
2024-06-20 20:08:08 +0000

    another C program that prints a quoted sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a necessary function which enables the program to compile
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
