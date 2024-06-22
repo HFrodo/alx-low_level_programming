c55d418d7ee8972d7638c8554179c0fb90ab7292
HFrodo <egyepong10@gmail.com>
2024-06-22 04:25:11 +0000

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of three digits + ','
+ * in ascending order
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int hundreds, tens, ones;
+
+	for (hundreds = 0; hundreds <= 7; hundreds++)
+	{
+		for (tens = hundreds + 1; tens <= 8; tens++)
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar(hundreds + '0');
+				putchar(tens + '0');
+				putchar(ones + '0');
+
+				if (hundreds != 7 || tens != 8 || ones != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
