791fea2a19b36068e309f82c7735b7886b59ca9f
HFrodo <egyepong10@gmail.com>
2024-06-28 23:49:25 +0000

    debugging main file 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,13 @@ int main(void)
 
 	i = 0;
 
-	/* Commenting out the infinite loop
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	*/
+	/**
+	 * Commenting out the infinite loop
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
