5b4102c0aeacaa5ff26885fdcf261738123a16cb
HFrodo <egyepong10@gmail.com>
2024-07-02 23:16:28 +0000

    function that prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s[10] = "My School";
+
+	_puts(s);
+	rev_string(s);
+	_puts(s);
+	return (0);
+}
