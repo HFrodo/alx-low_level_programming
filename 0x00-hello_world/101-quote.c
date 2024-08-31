d24f63a5465edb8a01a4ad0c4b3df1dd91ed251c
HFrodo <egyepong10@gmail.com>
2024-08-31 01:38:01 +0000

    a C program that prints exactly a specific sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 
+/**
+ * main - program starts
+ *
+ * Description: Prints the specified message to standard error and returns 1
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write (2, message, 59);
+	const char *message = "and that piece of art is useful\""
+		"- Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
 	return (1);
 }
